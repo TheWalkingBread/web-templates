6f5f181ff52720b293055d67a20e2a3890a7f9ea
TheWalkingBread <89442104+TheWalkingBread@users.noreply.github.com>
2023-09-27 03:52:46 +0800

    Include font import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,17 @@ import Image from 'next/image'
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
-import Preview from  '@/pages/preview'
+import Preview from '@/pages/preview'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Web Templates</title>
-    </Head>
-    <Preview/>
+      <Head>
+        <title>Web Templates</title>
+      </Head>
+      <Preview />
     </>
   )
 }
